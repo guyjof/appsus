2585c755df40d379918f9be3ddc3f59817832e83
Spinier <artsh94@gmail.com>
2021-06-26 21:33:18 +0300

    last day

---
+++ js/cmps/about-header.js
@@ -0,0 +1,56 @@
+// export default {
+//     template: `
+//             <header>
+//                 <div class="header-left">
+//                     <button>
+//                         <span class="material-icons">menu</span>
+//                     </button>
+//                     <div class="logo-container">
+//                         <div class="logo-img">
+//                             <img class="note-logo"src="img/google-keep-2-569459.png" alt="Miss Keep">
+//                         </div>
+//                         <div class="logo-text">
+//                             <p>Miss</p>
+//                             <p>Keep</p>
+//                         </div>
+//                     </div>
+//                 </div>
+
+//                 <div class="header-right">
+//                     <button @click="toggleModal"><span class="material-icons">apps</span></button>
+//                     <button><router-link to="/about"><span class="material-icons">account_circle</span></router-link></button>
+//                 </div>
+//                 <div v-if="isModalOpen" class="apps-modal">
+//                     <router-link to="/mail">
+//                         <div class="mail-modal-app">
+//                             <img src="img/Gmail_logo_PNG9.png" alt="Mister Mail">
+//                             <small>Mister Mail</small>
+//                         </div>
+//                     </router-link>
+//                     <router-link to="/note">
+//                         <div class="note-modal-app">
+//                             <img src="img/google-keep-2-569459.png" alt="Miss Keep">
+//                             <small>Miss Keep</small>
+//                         </div>
+//                     </router-link>
+//                     <router-link to="/book">
+//                         <div class="book-modal-app">
+//                             <img src="img/book.png" alt="Mister Mail">
+//                             <small>Miss Books</small>
+//                         </div>
+//                     </router-link>
+//                 </div>
+//             </header>
+//     `,
+//     data() {
+//         return {
+//             isModalOpen: false
+//         }
+//     },
+//     methods: {
+//         toggleModal() {
+//             this.isModalOpen = !this.isModalOpen
+//         }
+//     }
+
+// }
--- js/pages/about-page.js
+++ js/pages/about-page.js
@@ -1,7 +1,50 @@
+import appFooter from "../cmps/app-footer.js"
+// import aboutHeader from "../cmps/about-header.js";
+
 export default {
     template: `
         <section class="home-page app-main">
-            <h2 ref="header">About us...</h2>
+        <about-header/>
+        <div> 
+            <router-link> </router-link>
+        </div>
+
+        <div class="about-main"> 
+
+            <div class="profile-container">
+                <img class="profile-img" src="/img/profile/guy.jpg" alt="">
+                <h2 class="about-header" ref="header">Guy Jofre Gonzales</h2>
+                <p class="about-p"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quas rerum ullam aspernatur quidem quae incidunt earum, alias est blanditiis!</p>
+                <ul class="socials-container"> 
+
+
+                <a href="https://www.facebook.com/GUYJOFRE"><li><span><i class="fab fa-facebook">  </i>  </span></li></a>
+                <a href="https://github.com/guyjof"><li><span><i class="fab fa-github"></i> </span></li></a>
+                <a href="https://www.linkedin.com/in/guyjof/"><li><span><i class="fab fa-linkedin"></i>  </span></li></a>
+                </ul>
+            </div>
+            <div class="profile-container">
+                <img class="profile-img" src="/img/profile/arthur.jpg" alt="">
+                <h2 class="about-header" ref="header">Arthur Shtein</h2>
+                <p class="about-p"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quas rerum ullam aspernatur quidem quae incidunt earum, alias est blanditiis!</p>
+                <ul class="socials-container"> 
+                <a href="https://www.facebook.com/arthur.shtein.7/"><li><span><i class="fab fa-facebook">  </i>  </span></li></a>
+                <a href="https://github.com/Spinier"><li><span><i class="fab fa-github"></i> </span></li></a>
+                <a href="https://www.linkedin.com/in/arthur-shtein-1398111b4/"><li><span><i class="fab fa-linkedin"></i>  </span></li></a>
+                </ul>
+            </div>
+
+        </div>
+
+        <app-footer/>
+
         </section>
     `,
+
+
+    components: {
+        appFooter,
+        // aboutHeader
+    }
 };
+
